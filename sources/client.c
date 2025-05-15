3bf1d1924df898f0781100095df97e56d2e48744
root <root@reaktor-04.localdomain>
2025-05-15 18:55:12 +0200

    need to try this new version on 42 computers

--- sources/client.c
+++ sources/client.c
@@ -6,123 +6,229 @@
 /*   By: daniefe2 <daniefe2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 11:15:47 by daniefe2          #+#    #+#             */
-/*   Updated: 2025/04/21 08:57:30 by daniefe2         ###   ########.fr       */
+/*   Updated: 2025/05/08 19:35:26 by daniefe2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-volatile sig_atomic_t	g_server_confirm_reception = 0;
+// volatile sig_atomic_t	g_server_confirm_reception = 0;
 
-// Signal handler for acknowledgment from the server.
-// Sets a global flag when SIGUSR1 is received to indicate message was received.
-void	sig_handler(int sig)
+// // Signal handler for acknowledgment from the server.
+// // Sets a global flag when SIGUSR1 is received to indicate message was received.
+// void	sig_handler(int sig)
+// {
+// 	if (sig == SIGUSR1)
+// 	{
+// 		ft_printf("message received\n");
+// 		g_server_confirm_reception = 1;
+// 	}
+// }
+
+// // Converts a numeric string to an integer (PID).
+// // Returns the corresponding integer.
+// int	ft_atoi(char *str)
+// {
+// 	long	nb;
+// 	int		len;
+// 	int		i;
+
+// 	len = 0;
+// 	while (str && str[len])
+// 		len++;
+// 	i = 0;
+// 	nb = 0;
+// 	while (i < len)
+// 		nb = (nb * 10) + (str[i++] - '0');
+// 	return (nb);
+// }
+
+// // Sends a single bit to the server using SIGUSR1 (for 0) or SIGUSR2 (for 1).
+// // Pauses briefly after sending to ensure the server can process the signal.
+// void	send_bit(pid_t pid, int bit)
+// {
+// 	if (bit == 0)
+// 	{
+// 		if (kill(pid, SIGUSR1) == -1)
+// 		{
+// 			perror("Error sending SIGUSR1");
+// 			exit (1);
+// 		}
+// 	}
+// 	else
+// 	{
+// 		if (kill(pid, SIGUSR2) == -1)
+// 		{
+// 			perror("Error sending SIGUSR2");
+// 			exit(1);
+// 		}
+// 	}
+// 	usleep(300);
+// }
+
+// // Sends a message to the server bit by bit.
+// // Converts each character to binary and sends its bits using signals.
+// // Ends the message with a null character ('\0').
+// void	send_message(char *message, pid_t pid)
+// {
+// 	int				bit_tracker;
+// 	int				bit;
+// 	int				i;
+// 	unsigned char	c;
+
+// 	bit_tracker = 0;
+// 	while (message[bit_tracker] != '\0')
+// 	{
+// 		c = message[bit_tracker];
+// 		i = 7;
+// 		while (i >= 0)
+// 		{
+// 			bit = (c >> i--) & 1;
+// 			send_bit(pid, bit);
+// 		}
+// 		bit_tracker++;
+// 	}
+// 	c = (unsigned char) '\0';
+// 	i = 7;
+// 	while (i >= 0)
+// 	{
+// 		bit = (c >> i--) & 1;
+// 		send_bit(pid, bit);
+// 	}
+// }
+
+// // Parses arguments, sets up signal handler, validates PID,
+// // sends message to server.
+// // Waits for acknowledgment signal before exiting.
+// int	main(int argc, char **argv)
+// {
+// 	pid_t	pid;
+// 	char	*message;
+
+// 	if (argc != 3)
+// 		return (ft_printf("Usage: %s <server_pid> <message>\n", argv[0]), 1);
+// 	else
+// 	{
+// 		if (signal(SIGUSR1, sig_handler) == SIG_ERR)
+// 		{
+// 			ft_printf("Error with sig_handler() !\n");
+// 			exit(0);
+// 		}
+// 		pid = ft_atoi(argv[1]);
+// 		if (pid < 2 || pid > 4194304)
+// 			return (ft_printf("The PID is invalid. \n"), 1);
+// 		message = argv[2];
+// 		send_message(message, pid);
+// 		while (!g_server_confirm_reception)
+// 			pause();
+// 		ft_printf("Acknowledgment received.\n");
+// 		ft_printf("Message sent successfully to pid : %d\n.", pid);
+// 		return (0);
+// 	}
+// }
+
+//-----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void ack_handler(int signum)
 {
-	if (sig == SIGUSR1)
+	(void)signum;
+	// Just wakes up the pause() in send_char()
+}
+
+void	send_char(pid_t pid, unsigned char c)
+{
+	int	bit;
+
+	bit = 7;
+	while (bit >= 0)
 	{
-		ft_printf("message received\n");
-		g_server_confirm_reception = 1;
+		send_signal(pid, (c >> bit) & 1);
+		pause();  // wait for acknowledgment from server
+		bit--;
 	}
 }
 
-// Converts a numeric string to an integer (PID).
-// Returns the corresponding integer.
-int	ft_atoi(char *str)
-{
-	long	nb;
-	int		len;
-	int		i;
-
-	len = 0;
-	while (str && str[len])
-		len++;
-	i = 0;
-	nb = 0;
-	while (i < len)
-		nb = (nb * 10) + (str[i++] - '0');
-	return (nb);
-}
-
-// Sends a single bit to the server using SIGUSR1 (for 0) or SIGUSR2 (for 1).
-// Pauses briefly after sending to ensure the server can process the signal.
-void	send_bit(pid_t pid, int bit)
+void	send_signal(pid_t pid, int bit)
 {
 	if (bit == 0)
-	{
-		if (kill(pid, SIGUSR1) == -1)
-		{
-			perror("Error sending SIGUSR1");
-			exit (1);
-		}
-	}
+		kill(pid, SIGUSR1);  // Send SIGUSR1 for bit 1
 	else
-	{
-		if (kill(pid, SIGUSR2) == -1)
-		{
-			perror("Error sending SIGUSR2");
-			exit(1);
-		}
-	}
-	usleep(200);
+		kill(pid, SIGUSR2);  // Send SIGUSR2 for bit 0
 }
+// void	init_data(t_data *data)
+// {
+// 	int		input_length = 0;
+// 	char	*buffer = 0;
+// }
 
-// Sends a message to the server bit by bit.
-// Converts each character to binary and sends its bits using signals.
-// Ends the message with a null character ('\0').
-void	send_message(char *message, pid_t pid)
-{
-	int				bit_tracker;
-	int				bit;
-	int				i;
-	unsigned char	c;
-
-	bit_tracker = 0;
-	while (message[bit_tracker] != '\0')
-	{
-		c = message[bit_tracker];
-		i = 7;
-		while (i >= 0)
-		{
-			bit = (c >> i--) & 1;
-			send_bit(pid, bit);
-		}
-		bit_tracker++;
-	}
-	c = (unsigned char) '\0';
-	i = 7;
-	while (i >= 0)
-	{
-		bit = (c >> i--) & 1;
-		send_bit(pid, bit);
-	}
-}
-
-// Parses arguments, sets up signal handler, validates PID,
-// sends message to server.
-// Waits for acknowledgment signal before exiting.
 int	main(int argc, char **argv)
 {
-	pid_t	pid;
-	char	*message;
+	// data = init_data;
+	pid_t	pid_client;
+	int		i;
+
+	// assigning value to struct value is now working
+	// declare data = init data like in FdF?
+
+	struct sigaction sa_ack;
+	sa_ack.sa_handler = ack_handler;
+	sigemptyset(&sa_ack.sa_mask);
+	sa_ack.sa_flags = SA_RESTART;
 
 	if (argc != 3)
-		return (ft_printf("Usage: %s <server_pid> <message>\n", argv[0]), 1);
-	else
 	{
-		if (signal(SIGUSR1, sig_handler) == SIG_ERR)
-		{
-			ft_printf("Error with sig_handler() !\n");
-			exit(0);
-		}
-		pid = ft_atoi(argv[1]);
-		if (pid < 2 || pid > 4194304)
-			return (ft_printf("The PID is invalid. \n"), 1);
-		message = argv[2];
-		send_message(message, pid);
-		while (!g_server_confirm_reception)
-			pause();
-		ft_printf("Acknowledgment received.\n");
-		ft_printf("Message sent successfully to pid : %d\n.", pid);
-		return (0);
+		write(2, "Usage: ./client <PID> <message>\n", 33);
+		return (1);
 	}
+
+	// can i verify with sigaction if the pid signal is correct so i can print an error if necessary
+	// if (sigaction(SIGUSR1, pid) == SIG_ERR)
+	// {
+	// 	ft_printf("Error with sig_handler() !\n");
+	// 	exit(0);
+	// }
+
+	if ((pid_client = (pid_t)ft_atoi_strict(argv[1])) == 1)
+	{
+		perror("Incorrect PID!\n");
+		exit (EXIT_FAILURE);
+	}
+
+	// if (pid_client != pid_server)
+	// if (pid < 2 || pid > 4194304)
+	// {
+	// 	write(2, "The PID is invalid.\n", 21);
+	// 	return (1);
+	// }
+
+	if (sigaction(SIGUSR1, &sa_ack, NULL) == -1)
+	{
+		perror("sigaction (ACK)");
+		exit(EXIT_FAILURE);
+	}
+
+	// Loop through the string and send each character
+	i = 0;
+	while (argv[2][i] != '\0')
+	{
+		send_char(pid_client, argv[2][i]);  // Send the character one by one
+		i++;
+	}
+
+	// Send a null character '\0' to mark the end of the string
+	send_char(pid_client, '\0');
+	return (0);
 }
--- sources/utils.c
+++ sources/utils.c
@@ -6,7 +6,7 @@
 /*   By: daniefe2 <daniefe2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 11:58:46 by daniefe2          #+#    #+#             */
-/*   Updated: 2025/04/19 10:40:17 by daniefe2         ###   ########.fr       */
+/*   Updated: 2025/05/01 10:16:43 by daniefe2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,93 @@
 	// No initialization of buffer - it's not strictly necessary
 	// since we'll only read up to the index we've written to
 	// Creates a new node for the linked list used in buffering characters.
-t_node	*create_node(void)
-{
-	t_node	*node;
+// t_node	*create_node(void)
+// {
+// 	t_node	*node;
 
-	node = malloc(sizeof(t_node));
-	if (!node)
-	{
-		ft_printf("Error allocating node.\n");
-		return (NULL);
-	}
-	node->index = 0;
-	node->next = NULL;
-	return (node);
-}
+// 	node = malloc(sizeof(t_node));
+// 	if (!node)
+// 	{
+// 		ft_printf("Error allocating node.\n");
+// 		return (NULL);
+// 	}
+// 	node->index = 0;
+// 	node->next = NULL;
+// 	return (node);
+// }
 
 // Prints all characters from the linked list buffer to standard output.
 // Frees each node in the linked list after printing.
-void	buffer_print_and_free(t_node *head)
-{
-	t_node	*tmp;
-	t_node	*to_free;
+// void	buffer_print_and_free(t_node *head)
+// {
+// 	t_node	*tmp;
+// 	t_node	*to_free;
 
-	tmp = head;
-	while (tmp)
-	{
-		write(1, tmp->buffer, tmp->index);
-		to_free = tmp;
-		tmp = tmp->next;
-		free(to_free);
-	}
-	write(1, "\n", 1);
+// 	tmp = head;
+// 	while (tmp)
+// 	{
+// 		write(1, tmp->buffer, tmp->index);
+// 		to_free = tmp;
+// 		tmp = tmp->next;
+// 		free(to_free);
+// 	}
+// 	write(1, "\n", 1);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int	ft_strlen_00(char *str)
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
+int	calculate_delay(int len)
+{
+	if (len < 100)
+		return (50);
+	else if (len < 500)
+		return (100);
+	else if (len < 1000)
+		return (200);
+	return (300);
+}
+
+int	ft_atoi_strict(char *str)
+{
+	int		i;
+	long	nb;
+
+	if (!str || !str[0])
+		return (0);
+	i = 0;
+	nb = 0;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (1);
+		nb = (nb * 10) + (str[i] - '0');
+		if (nb > 2147483647)
+			return (1);
+		i++;
+	}
+	return ((int)nb);
+}
+
